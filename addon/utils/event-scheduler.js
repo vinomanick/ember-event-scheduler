b3291c80426c0a072d34500a7a8809c1b0112754
Vinodh Kumar <vinodhkumar.manickam@freshworks.com>
2020-03-05 07:21:16 +0530

    Calendar layout update

--- addon/utils/event-scheduler.js
+++ addon/utils/event-scheduler.js
@@ -1,5 +1,4 @@
 import moment from 'moment';
-import { VIEWS } from '../constants/event-scheduler';
 import { getTimeRange } from './date-util';
 import CalendarEvent from '../_private/classes/calendar-event';
 
@@ -32,37 +31,49 @@ const getSlots = (selectedDate, options) => {
   return getTimeRange(durationObj, offsetDate, interval);
 };
 
-const getCompactEventTime = (eventStartTime, eventEndTime, momentService) => {
-  let _momentService = momentService || moment;
-  if (eventStartTime && eventEndTime) {
-    let startTime = _momentService.moment(eventStartTime);
-    let endTime = _momentService.moment(eventEndTime);
-    let [startHr, startMn, startMD] = startTime.format('h mm a').split(' ');
-    let [endHr, endMn, endMD] = endTime.format('h mm a').split(' ');
-    let startHM = parseInt(startMn) ? `${startHr}:${startMn}` : startHr;
-    let endHM = parseInt(endMn) ? `${endHr}:${endMn}` : endHr;
-    if (startTime.isSame(endTime, 'day')) {
-      return startMD === endMD ? `${startHM} - ${endHM}${endMD}` : `${startHM}${startMD} - ${endHM}${endMD}`;
-    } else {
-      let startDay = `${startTime.format('D MMM')} ${startHM}${startMD}`;
-      let endDay = `${endTime.format('D MMM')} ${endHM}${endMD}`;
-      return `${startDay} - ${endDay}`;
+const getCalendarPeriod = (startTime, endTime, dayFormat, _moment) => {
+  let _momentService = _moment || moment;
+  if (startTime && endTime) {
+    let _startTime = _momentService.moment(startTime);
+    let _endTime = _momentService.moment(endTime);
+    if (_startTime.isSame(_endTime, 'day')) {
+      _startTime.format(dayFormat);
+    } else if (_startTime.isSame(_endTime, 'month')) {
+      _startTime.format('MMMM YYYY');
+    } else if (_startTime.isSame(_endTime, 'year')) {
+      return `${_startTime.format('MMM')} - ${_endTime.format('MMM YYYY')}`;
     }
+    return `${_startTime.format('MMM YYYY')} - ${_endTime.format('MMM YYYY')}`;
   }
 };
 
-const getCurrentPeriod = (viewType, selectedDate, dayDateFormat = 'DD MMMM YYYY') => {
-  if (viewType === VIEWS.DAY) {
-    return selectedDate.format(dayDateFormat);
-  } else {
-    let startDay = selectedDate.clone().startOf('week');
-    let endDay = selectedDate.clone().endOf('week');
-    if (!startDay.isSame(endDay, 'year')) {
-      return `${startDay.format('MMM YYYY')} - ${endDay.format('MMM YYYY')}`;
-    } else if (!startDay.isSame(endDay, 'month')) {
-      return `${startDay.format('MMM')} - ${endDay.format('MMM YYYY')}`;
+const getEventPeriod = (startTime, endTime, dateTimeFormat, timeFormat, _moment) => {
+  let _momentService = _moment || moment;
+  if (startTime && endTime) {
+    let _startTime = _momentService.moment(startTime);
+    let _endTime = _momentService.moment(endTime);
+    return _startTime.isSame(_endTime, 'day')
+      ? `${_startTime.format(dateTimeFormat)} -  ${_endTime.format(timeFormat)}`
+      : `${_startTime.format(dateTimeFormat)} -  ${_endTime.format(dateTimeFormat)}`
+  }
+};
+
+const getEventCompactPeriod = (startTime, endTime, _moment) => {
+  let _momentService = _moment || moment;
+  if (startTime && endTime) {
+    let _startTime = _momentService.moment(startTime);
+    let _endTime = _momentService.moment(endTime);
+    let [startHr, startMn, startMD] = _startTime.format('h mm a').split(' ');
+    let [endHr, endMn, endMD] = _endTime.format('h mm a').split(' ');
+    let startHM = parseInt(startMn) ? `${startHr}:${startMn}` : startHr;
+    let endHM = parseInt(endMn) ? `${endHr}:${endMn}` : endHr;
+    if (_startTime.isSame(_endTime, 'day')) {
+      return startMD === endMD ? `${startHM} - ${endHM}${endMD}` : `${startHM}${startMD} - ${endHM}${endMD}`;
+    } else {
+      let startDay = `${_startTime.format('D MMM')} ${startHM}${startMD}`;
+      let endDay = `${_endTime.format('D MMM')} ${endHM}${endMD}`;
+      return `${startDay} - ${endDay}`;
     }
-    return startDay.format('MMMM YYYY');
   }
 };
 
@@ -80,6 +91,6 @@ const getTimerPos = (durationInMins, slotConfig) => {
   return (durationInMins * width) / intervalInMins;
 };
 
-export { getSlots, buildCalendarEvent,
-   getCompactEventTime, getCurrentPeriod, getTimerPos,
+export { getSlots, buildCalendarEvent, getTimerPos,
+  getCalendarPeriod, getEventPeriod, getEventCompactPeriod,
   getCustomResourceId, getCustomEventId };
