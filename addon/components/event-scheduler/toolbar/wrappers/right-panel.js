f50a704be317a0b62289d1c3fdae0789d0717953
Vino <vino1393@gmail.com>
2020-04-26 09:14:53 +0530

    Fixed duration issue

--- addon/components/event-scheduler/toolbar/wrappers/right-panel.js
+++ addon/components/event-scheduler/toolbar/wrappers/right-panel.js
@@ -1,14 +1,21 @@
 import Component from '@ember/component';
 import layout from '../../../../templates/components/event-scheduler/toolbar/wrappers/right-panel';
 import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
   moment: service(),
   intl: service(),
   layout,
+  durationOptions: reads('durationConfig.options'),
   viewsList: computed(function() {
     return Object.keys(this.views);
+  }),
+
+  durationSelected: computed('selectedDuration', function() {
+    let { format: selectedFormat, value: selectedValue } = this.selectedDuration;
+    return this.durationOptions.find(({ format, value }) => format === selectedFormat && value === selectedValue);
   }),
 
   classNames: ['es-toolbar__panel', 'align-items--center'],
