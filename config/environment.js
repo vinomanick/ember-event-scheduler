7face777adf8c5ce138c7f3ac5f762a018e7081a
Vinodh Kumar <vino1393@gmail.com>
2020-05-03 14:09:57 +0530

    Updated moment locales

--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,10 @@
 
 module.exports = function(/* environment, appConfig */) {
   return {
+    moment: {
+      includeLocales: ['ar', 'ca', 'cs', 'da', 'de', 'es-do', 'es', 'et', 'fi', 'fr', 'he', 'hu', 'id', 'it', 'ja', 'ko', 'nb', 'nl', 'pl', 'pt-br', 'pt', 'ro', 'ru', 'sk', 'sl', 'sv', 'th', 'tr', 'uk', 'vi', 'zh-cn', 'zh-tw', 'lv', 'bs', 'bg', 'hr', 'el', 'ms', 'lt', 'sr', 'is', 'tl-ph'],
+      includeTimezone: 'all',
+      allowEmpty: true // default: false
+    }
   };
 };
