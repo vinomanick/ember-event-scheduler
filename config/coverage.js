509435467c45e063c07dd44871a83d8f3e1ca585
Vinodh Kumar <vino1393@gmail.com>
2020-05-03 16:10:45 +0530

    Ignored tests suite coverage

---
+++ config/coverage.js
@@ -0,0 +1,12 @@
+module.exports = {
+  excludes: [
+    '*/mirage/**/*',
+    '*/tests/**/*',
+    '*/vendor/**/*',
+    '*/app/styles/**/*',
+    '*/lib/**/*',
+    '*/assets/**/*',
+    '*/config/**/*'
+  ]
+  // parallel: true
+};
